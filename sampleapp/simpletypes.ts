d13e3d24bbf3c00ec158fa6d9180c2ec38bb60b2
DESKTOP-5S2QARB\eCounselling382 <pandey.vikas@nic.in>
2022-10-12 18:08:58 +0530

    simple types

---
+++ sampleapp/simpletypes.ts
@@ -0,0 +1,8 @@
+let u=true;
+//u="hi"; ////Argument of type 'boolean' is not assignable to parameter of type 'string'
+//Math.round(u); //Argument of type 'boolean' is not assignable to parameter of type 'number'.
+
+let x:any=true;
+x = 5;
+x = "5s";
+console.log(x);
